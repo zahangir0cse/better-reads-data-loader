4ff0d230a56ed1cb4380c2dd43b36a374eef79d5
Zahangir Alam <zahangir0cse@gmail.com>
2022-03-23 18:46:26 +0600

    init project

---
+++ src/main/java/com/iict/buet/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.iict.buet.betterreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterReadsDataLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iict/buet/betterreadsdataloader/BetterReadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iict.buet.betterreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterReadsDataLoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
