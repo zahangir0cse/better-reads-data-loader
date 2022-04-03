052fdd8cf2d2eedc74fe62c0bd48043484188e8e
Zahangir Alam <zahangir0cse@gmail.com>
2022-04-03 17:59:47 +0600

    add author model

---
+++ src/main/java/com/iict/buet/betterreadsdataloader/model/Author.java
@@ -0,0 +1,23 @@
+package com.iict.buet.betterreadsdataloader.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table("author_by_id")
+@Data
+public class Author {
+    @Id
+    @PrimaryKeyColumn(name = "author_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    private String id;
+    @Column("author_name")
+    @CassandraType(type = CassandraType.Name.TEXT)
+    private String name;
+    @Column("personal_name")
+    @CassandraType(type = CassandraType.Name.TEXT)
+    private String personalName;
+}
